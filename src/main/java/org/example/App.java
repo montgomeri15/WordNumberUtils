4aee3de83f204bcb2d80666d731443d726041bc8
montgomeri15 <montgomeri15@gmail.com>
2022-11-21 17:42:06 +0200

    First commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main( String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/WordUtils.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.List;
+import java.util.Optional;
+
+public class WordUtils {
+
+
+}
+
+
+/**
+ *Створити Клас WordUtils у якого є метод:
+ * getShortestWord(List words) - знаходить найкоротше слово
+ * порахувати кількість входжень слів у речення
+ *
+ * Створити Клас NumberUtils у якого є методи які:
+ * getSum(List numbers) - Порахувати суму чисел
+ * multiplyOddNumber(List numbers) - знаходить непарні числа і множить на 2 і повертає список помножених чисел.
+ * Приклад : 1, 2, 3 -> 2, 6
+ * */
